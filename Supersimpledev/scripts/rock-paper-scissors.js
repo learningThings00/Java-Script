cb08bd4715407dc4409b281004b4168bc075911d
learningThings00 <mdanaskhan4567@gmail.com>
2026-04-23 17:30:20 +0530

    Day 23
    Today I revised i learned before and improved my projects especially todo one by adding map and filter method to it and using elvent listeners instead of onclick attribute

--- Supersimpledev/scripts/rock-paper-scissors.js
+++ Supersimpledev/scripts/rock-paper-scissors.js
@@ -18,7 +18,7 @@ let score = JSON.parse(localStorage.getItem("score")) || {
       function autoPlay() {
         
         if (!isPlaying) {
-          intervalId = setInterval(function() {
+          intervalId = setInterval(() => {
             const playerMove = randomMove();
             playGame(playerMove);
         }, 1000);
@@ -45,6 +45,33 @@ let score = JSON.parse(localStorage.getItem("score")) || {
           return "scissors";
         }
       }
+
+      document.querySelector('.js-rock-button')
+      .addEventListener('click', () =>  playGame('rock'));
+
+      document.querySelector('.js-paper-button')
+      .addEventListener('click', () =>  playGame('paper'));
+
+      document.querySelector('.js-scissors-button')
+      .addEventListener('click', () =>  playGame('scissors'));
+
+      document.querySelector('.js-reset-button')
+      .addEventListener('click', () => resetScore());
+
+      document.querySelector('.js-auto-play-button')
+      .addEventListener('click', () => autoPlay());
+
+
+
+      document.body.addEventListener('keydown', (event) => {
+        if (event.key === 'r') {
+          playGame('rock');
+        } else if (event.key === 'p') {
+          playGame('paper');
+        } else if (event.key === 's') {
+          playGame('scissors');
+        }
+      });
 
       function playGame(playerMove) {
         const computerMove = randomMove();
--- Supersimpledev/scripts/todo.js
+++ Supersimpledev/scripts/todo.js
@@ -2,6 +2,9 @@ let todoList =  JSON.parse(localStorage.getItem('todoLog')) ||  [];
 
 renderTodoList();//render means to display something on the page
 
+document.querySelector('.js-add-todo')
+.addEventListener('click', () => addTodo());
+
 function addTodo() {
   const nameInputElement = document.querySelector('.js-todo');
   const name = nameInputElement.value;
@@ -13,12 +16,15 @@ function addTodo() {
 
   if (name && date) {
 
+    const id = crypto.randomUUID();
+
     todoList.push({
     //shorthand for this is we can use single name instead of name: name(variable). called shorthand property syntax
     // name: name,
     // date: date
     name,
-    date
+    date,
+    id
   });
 
   updateLocalStorage();
@@ -30,6 +36,7 @@ function addTodo() {
   nameInputElement.value = '';
   dateInputElement.value = '';
 
+  console.log(todoList);
   } else {
     if (!name && !date) {
       errorMessage.innerHTML = 'Enter Todo and date first';
@@ -50,23 +57,31 @@ function addTodoKeydown(event) {
 
 //the technique used inside the fn is called generating HTML
 function renderTodoList() {
+
+  let todoListHTML = todoList.map((todo) => {
+    return `
+    <div>${todo.name}</div>
+    <div>${todo.date}</div>
+    <button class="delete-button js-delete-todo-button" data-id="${todo.id}">Delete</button>
+    `;
+  }).join('');
+  
+  //above code is a better way to display an html
+
+  /*
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, i) {
+  todoList.forEach((todoObject, i) => {
  const { name, date } = todoObject;
 
  const todoHTML = `
  <div>${name}</div>
  <div>${date}</div>
- <button onclick="
- todoList.splice(${i}, 1);
- renderTodoList();
- updateLocalStorage();
- " class="delete-button">Delete</button>
+ <button class="delete-button js-delete-todo-button">Delete</button>
  `;
   todoListHTML += todoHTML;   
   });
-
+*/
 // for (let i = 0; i < todoList.length; i++) {
 //  const todoObject = todoList[i];
 // //  const name = todoObject.name;
@@ -87,6 +102,20 @@ function renderTodoList() {
 //   todoListHTML += todoHTML;
 // }
  document.querySelector('.js-todo-display').innerHTML = todoListHTML;
+
+ document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, i) => {
+deleteButton.addEventListener('click', () => {
+  const deleteId = deleteButton.dataset.id;
+ todoList = todoList.filter((todo) => {
+   return deleteId !== todo.id;
+    }
+  )
+  //todoList.splice(i, 1);
+ renderTodoList();
+ updateLocalStorage();
+ })
+ });
+ 
 }
 
 function clearAll() {
@@ -108,4 +137,3 @@ function clearAll() {
 function updateLocalStorage() {
   localStorage.setItem('todoLog', JSON.stringify(todoList));
 }
-
